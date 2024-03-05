6b85f3007a8ed0a6cac769f6092c947b89d08e32
aravindtitle <aravindtitle@gmail.com>
2024-03-05 08:23:13 +0530

    Add to Cart Fixes

--- src/Store/cart-slice.js
+++ src/Store/cart-slice.js
@@ -4,15 +4,16 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
-    totalquantity: 0,
+    totalQuantity: 0,
   },
   reducers: {
     addItemToCart(state, action) {
-      const item = action.payload;
-      const existingItem = state.item.find((item) => item.id === newItem.id);
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -23,6 +24,19 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,16 +1,20 @@
-import classes from "./CartButton.module.css";
-import { UseDispatch, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+
 import { uiActions } from "../../Store/ui-slice";
+import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+
   const toggleCartHandler = () => {
     dispatch(uiActions.toggle());
   };
+
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
